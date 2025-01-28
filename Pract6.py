1cfe1156cd5164dfcd7933baf4e8f8799592b73a
aaobhai <drop141111@gmail.com>
2025-01-28 09:37:11 +0530

    Update Pract6.py

--- Pract6.py
+++ Pract6.py
@@ -1,20 +1,14 @@
+from pyspark import SparkConf, SparkContext 
 
-#pract 6 !pip install pyspark
-
-from pyspark import SparkConf, SparkContext
-
-conf = SparkConf().setMaster("local").setAppName("TotalTimeSpend")
-sc = SparkContext(conf = conf)
-
-rdd = sc.parallelize(range(10))
-a_big_list = [1, 5]
-result = rdd.map(lambda x : (x, x in a_big_list)).collect()
-
-
-a_big_list_br = sc.broadcast(a_big_list)
-# Use a_big_list_br.value instead of a_big_list.value
-result = rdd.map(lambda x : (x, x in a_big_list_br.value)).collect()
-print(result[2])
+conf = SparkConf().setMaster("local").setAppName("SpendByCustomer") sc = SparkContext(conf=conf) 
+def extractCustomerPricePairs(line): 
+ fields = line.split(',') 
+ return (int(fields[0]), float(fields[2])) 
+input = sc.textFile("customer-orders.csv") 
+mappedInput = input.map(extractCustomerPricePairs) 
+totalByCustomer = mappedInput.reduceByKey(lambda x, y: x + y) results = totalByCustomer.collect() 
+for result in results: 
+ print(result)
 
 
 
